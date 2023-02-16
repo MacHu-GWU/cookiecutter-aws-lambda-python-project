e967c8811c4283e390427209adb3e068f8c73aa7
Sanhe Hu <husanhe@gmail.com>
2023-02-15 21:54:19 -0500

    Update boto_ses.py

--- {{ cookiecutter.package_name }}-project/{{ cookiecutter.package_name }}/tests/boto_ses.py
+++ {{ cookiecutter.package_name }}-project/{{ cookiecutter.package_name }}/tests/boto_ses.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from boto_session_manager import BotoSesManager, AwsServiceEnum
+from boto_session_manager import BotoSesManager
 
-aws_profile = "aws_data_lab_sanhe_us_east_1"
-
-bsm = BotoSesManager(profile_name=aws_profile)
-cf_client = bsm.get_client(AwsServiceEnum.CloudFormation)
+bsm = BotoSesManager(
+    profile_name="{{ cookiecutter.aws_profile }}",
+    region_name="{{ cookiecutter.aws_region }}",
+)
