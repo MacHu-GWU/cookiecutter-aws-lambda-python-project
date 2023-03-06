ba6ede1cfb0c0c33b8b77d1dfb275461109a8da3
Sanhe Hu <husanhe@gmail.com>
2023-03-06 11:53:24 -0500

    Create generate_example_project.py

---
+++ generate_example_project.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+I would like to include an example project that is generated from this template.
+This script can automatically generate the example project.
+"""
+
+import shutil
+from pathlib import Path
+from cookiecutter.main import cookiecutter
+
+
+dir_here: Path = Path(__file__).absolute().parent
+dir_output = dir_here.joinpath("aws_lambda_python_example-project")
+
+shutil.rmtree(dir_output, ignore_errors=True)
+cookiecutter(
+    template=f"{dir_here}",
+    output_dir=f"{dir_output}",
+    extra_context={
+        "package_name": "aws_lambda_python_example",
+        "author_name": "Firstname Lastname",
+        "author_email": "firstname.lastname@email.com",
+        "semantic_version": "0.1.1",
+        "aws_profile": "my_aws_profile",
+        "aws_account_id": "111122223333",
+        "aws_region": "us-east-1",
+    },
+    no_input=True,
+)
